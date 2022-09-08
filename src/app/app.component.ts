6dc97de16239c4cbb556079551f3c1a0b7f90b78
abdullahghous02 <ghousabdullah1997@gmail.com>
2022-09-08 15:02:25 +0500

    changes today

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,13 +47,13 @@ export class AppComponent {
 
   public evenImgs: any[] = [
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/2.jpg',
+      img: '../assets/a2.jpg',
       index: 2
     }
   ];
   public oddImgs: any[] = [
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/1.jpg',
+      img: '../assets/a1.jpg',
       index: 1
     }
   ];
@@ -63,6 +63,10 @@ export class AppComponent {
   index: any = '0 - 1 / 30';
   nextbtn1 = true;
   nextbtn2 = false;
+
+  prebtn1 = true;
+  prebtn2 = false;
+
   data:any;
 
   // ngAfterViewInit(): void {
@@ -115,7 +119,6 @@ export class AppComponent {
     const newImgIndexOdd = newImgIndexEven + 1;
 
     if(newImgIndexEven === 32) {
-      console.log('ddddqwqwqwq');
       this.nextbtn1 = false;
       this.nextbtn2 = true;
     }
@@ -155,159 +158,182 @@ export class AppComponent {
     const newImgIndexEven = id - 1;
     const newImgIndexOdd = newImgIndexEven - 1;
 
+    this.index = `${newImgIndexEven} - ${newImgIndexEven === 30 ? 0 : newImgIndexOdd}`;
 
-    console.log(newImgIndexEven, newImgIndexOdd, 'even', 'odd');
-
-    this.index = `${newImgIndexEven} - ${newImgIndexEven === 30 ? 0 : newImgIndexOdd}`
-
-    if(newImgIndexEven == 2 && newImgIndexOdd == 1) {
-      console.log('index 2 - 1');
-      this.allImages.forEach((even) => {
-        if(even.index == 2) {
-          this.evenImgs.push(even)
-        }
-      })
-  
-      this.allImages.forEach((odd) => {
-        if(odd.index == 1) {
-          this.oddImgs.push(odd)
-        }
-      })
-    }else if(newImgIndexEven !== 2 && newImgIndexOdd !== 1) {
-      console.log('index 1 - -0');
-      this.allImages.forEach((even) => {
-        if(even.index == newImgIndexEven) {
-          this.evenImgs.push(even)
-        }
-      })
-  
-      this.allImages.forEach((odd) => {
-        if(odd.index == newImgIndexOdd) {
-          this.oddImgs.push(odd)
-        }
-      })
+    if(newImgIndexEven === 2) {
+      console.log('ddddqwqwqwq');
+      this.index = '0 - 1 / 30';
+      this.prebtn1 = false;
+      this.prebtn2 = true;
+      this.firstDiv = 1;
+      this.showDoubleImg = 1;
+      this.showIndexFirst = 1;
     }
+    else {
+      this.prebtn1 = true;
+      this.prebtn2 = false;
+    }
+
+
+    this.allImages.forEach((even) => {
+      if(even.index == newImgIndexEven) {
+        this.evenImgs.push(even)
+      }
+    })
+
+    this.allImages.forEach((odd) => {
+      if(odd.index == newImgIndexOdd) {
+        this.oddImgs.push(odd)
+      }
+    })
+
+    // if(newImgIndexEven == 2 && newImgIndexOdd == 1) {
+    //   this.allImages.forEach((even) => {
+    //     if(even.index == 2) {
+    //       this.evenImgs.push(even)
+    //     }
+    //   })
+  
+    //   this.allImages.forEach((odd) => {
+    //     if(odd.index == 1) {
+    //       this.oddImgs.push(odd)
+    //     }
+    //   })
+    // }else if(newImgIndexEven !== 2 && newImgIndexOdd !== 1) {
+    //   console.log('index 1 - -0');
+    //   this.allImages.forEach((even) => {
+    //     if(even.index == newImgIndexEven) {
+    //       this.evenImgs.push(even)
+    //     }
+    //   })
+  
+    //   this.allImages.forEach((odd) => {
+    //     if(odd.index == newImgIndexOdd) {
+    //       this.oddImgs.push(odd)
+    //     }
+    //   })
+    // }
   }
 
   public allImages = [
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/1.jpg',
+      img: '../assets/a1.jpg',
       index: 1
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/2.jpg',
+      img: '../assets/a2.jpg',
       index: 2
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/3.jpg',
+      img: '../assets/a3.jpg',
       index: 3
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/4.jpg',
+      img: '../assets/a4.jpg',
       index: 4
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/5.jpg',
+      img: '../assets/a5.jpg',
       index: 5
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/6.jpg',
+      img: '../assets/a6.jpg',
       index: 6
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/7.jpg',
+      img: '../assets/a7.jpg',
       index: 7
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/8.jpg',
+      img:'../assets/a8.jpg',
       index: 8
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/9.jpg',
+      img:'../assets/a9.jpg',
       index: 9
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/10.jpg',
+      img: '../assets/a10.jpg',
       index: 10
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/11.jpg',
+      img: '../assets/a11.jpg',
       index: 11
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/12.jpg',
+      img: '../assets/a12.jpg',
       index: 12
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/13.jpg',
+      img: '../assets/a13.jpg',
       index: 13
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/14.jpg',
+      img: '../assets/a14.jpg',
       index: 14
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/15.jpg',
+      img: '../assets/a15.jpg',
       index: 15
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/16.jpg',
+      img: '../assets/a16.jpg',
       index: 16
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/17.jpg',
+      img: '../assets/a17.jpg',
       index: 17
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/18.jpg',
+      img: '../assets/a18.jpg',
       index: 18
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/19.jpg',
+      img: '../assets/a19.jpg',
       index: 19
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/20.jpg',
+      img: '../assets/a20.jpg',
       index: 20
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/21.jpg',
+      img: '../assets/a21.jpg',
       index: 21
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/22.jpg',
+      img: '../assets/a22.jpg',
       index: 22
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/23.jpg',
+      img: '../assets/a23.jpg',
       index: 23
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/24.jpg',
+      img: '../assets/a24.jpg',
       index: 24
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/25.jpg',
+      img: '../assets/a25.jpg',
       index: 25
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/26.jpg',
+      img: '../assets/a26.jpg',
       index: 26
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/27.jpg',
+      img: '../assets/a27.jpg',
       index: 27
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/28.jpg',
+      img: '../assets/a28.jpg',
       index: 28
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/29.jpg',
+      img: '../assets/a29.jpg',
       index: 29
     },
     {
-      img: 'https://static01.eu/1prospekte.de/images/uploads/folder/87858/30.jpg',
+      img: '../assets/a30.jpg',
       index: 30
     }
   ]
